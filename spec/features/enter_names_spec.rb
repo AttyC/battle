2f5e0a54c99c6355b20fc925e8ab00a8d3c826f6
Anjana <anjana.lpn@gmail.com>
2018-01-17 17:21:51 +0000

    update spec files

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -9,3 +9,11 @@ feature 'names entered into form' do
     expect(page).to have_text('Alice Bob')
   end
 end
+
+feature 'see player2 hit points'do
+  scenario 'player1 to be able to view player2 hit points'do
+    visit('/play')
+    find('.hitpoints').visible?
+    within('.hitpoints') { expect(page).to have_text(23) }
+  end
+end
