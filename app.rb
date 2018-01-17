2f907a4eec2e245e08d10c9f285c9d263d74aa70
Anjana <anjana.lpn@gmail.com>
2018-01-17 15:26:37 +0000

    add tests for form

--- app.rb
+++ app.rb
@@ -2,10 +2,17 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
-  get '/' do
-    "Testing infrastructure working!"
-  end
+
 
   # run! if app_file == $0
+  get '/' do
+    erb(:index)
+  end
 
+  post '/names' do
+    p params
+    @player1 = params[:player1]
+    @player2 = params[:player2]
+    erb(:play)
+  end
 end
--- spec/features/testing_infrastructre_spec.rb
+++ spec/features/testing_infrastructre_spec.rb
@@ -1,8 +1,19 @@
 #write a feature test that checks that the hompage says Testing infrastructure working!.
 
-feature 'Testing homepage' do
-  scenario 'app loads homepage with correct message' do
+# feature 'Testing homepage' do
+#   scenario 'app loads homepage with correct message' do
+#     visit('/')
+#     expect(page).to have_text('Testing infrastructure working!')
+#   end
+# end
+
+feature 'names entered into form' do
+  scenario 'players names are entered into a form' do
     visit('/')
-    expect(page).to have_text('Testing infrastructure working!')
+    find('form').visible?
+    fill_in 'player1', with: 'Alice'
+    fill_in 'player2', with: 'Bob'
+    click_button 'Submit'
+    expect(page).to have_text('Alice Bob')
   end
 end
