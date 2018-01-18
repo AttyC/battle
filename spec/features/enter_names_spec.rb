e1d46642006d9e3d4e310837ddee8337065ce76a
Antonia Cronin <antoniajcronin@gmail.com>
2018-01-18 10:57:19 +0000

    added test helper

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,18 +1,14 @@
 
 feature 'names entered into form' do
   scenario 'players names are entered into a form' do
-    visit('/')
-    find('form').visible?
-    fill_in 'player1', with: 'Alice'
-    fill_in 'player2', with: 'Bob'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_text('Alice Bob')
   end
 end
 
 feature 'see player2 hit points'do
   scenario 'player1 to be able to view player2 hit points'do
-    visit('/play')
+    sign_in_and_play
     find('.hitpoints').visible?
     within('.hitpoints') { expect(page).to have_text(23) }
   end
