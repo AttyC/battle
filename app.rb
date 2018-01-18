4218fa8859d4365e3e9f1d6204cf5d42bfeeddbc
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-01-18 14:16:40 +0000

    Add global variables

--- app.rb
+++ app.rb
@@ -3,9 +3,6 @@ require './lib/player'
 
 class Battle < Sinatra::Base
 
-  $player1 = Player.new('Alice')
-  $player2 = Player.new('Bob')
-
   enable :sessions
 
   get '/' do
@@ -13,22 +10,21 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    p params
-    session[$player1] = params[$player1]
-    session[$player2] = params[$player2]
+    $player1 = Player.new(params[:player1_name])
+    $player2 = Player.new(params[:player2_name])
     redirect to('/play')
   end
 
   get '/play' do
-    $player1 = session[$player1]
-    $player2= session[$player2]
+    @player1_name = $player1.name
+    @player2_name = $player2.name
     erb(:play)
 
   end
 
   get '/attack' do
-    $player1 = session[$player1]
-    $player2= session[$player2]
+    @player1_name = $player1.name
+    @player2_name = $player2.name
     erb(:attack)
   end
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,6 +1,6 @@
 def sign_in_and_play
   visit('/')
-  fill_in 'player1', with: 'Alice'
-  fill_in 'player2', with: 'Bob'
+  fill_in 'player1_name', with: 'Alice'
+  fill_in 'player2_name', with: 'Bob'
   click_button 'Submit'
 end
