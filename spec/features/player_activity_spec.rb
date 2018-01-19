e5d7b3ce06bdce281b1eff59af3b244f4546f385
Jennifer <jennifer.elaine.mairead@gmail.com>
2018-01-19 11:58:10 +0000

    tests that both HP are shown in player view

--- spec/features/player_activity_spec.rb
+++ spec/features/player_activity_spec.rb
@@ -1,7 +1,12 @@
 feature 'see player2 hit points'do
-  scenario 'player1 to be able to view player2 hit points'do
+  scenario 'view player2 hit points' do
     sign_in_and_play
-    within('.hitpoints') { expect(page).to have_text(100) }
+    within('.hitpoints') { expect(page).to have_text('Bob has 100 points') }
+  end
+
+  scenario 'view player1 hit points' do
+    sign_in_and_play
+    within('.hitpoints') { expect(page).to have_text('Alice has 100 points') }
   end
 end
 
